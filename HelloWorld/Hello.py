5e6ffbf4edf3686ba4999d5ae828c3c1af63b883
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-05 23:08:12 +0530

    Basic Input

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -1,3 +1,11 @@
-message = "----- Hello Python ----- "
+import sys
+
+message = "----- Person Details ----- "
 
 print(message)
+
+userName = input("Enter your Name: ")
+print(userName)
+
+userAge = int(input("Enter your Age: "))
+print(userAge+10)

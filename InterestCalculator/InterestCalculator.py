1007bc1770121920b1d38e61836519376fc4c4f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-06 22:49:29 +0530

    little formatting

--- InterestCalculator/InterestCalculator.py
+++ InterestCalculator/InterestCalculator.py
@@ -2,7 +2,7 @@ print('----- Interest Calculator -----')
 
 principalAmount = float(input('Principal amount: '))
 interest = float(input('Rate of Interest: '))
-years = int(input('Duration (no. of years) ?'))
+years = int(input('Duration (no. of years): '))
 
 total = (principalAmount * pow(1 + (interest/100), years))
 interest = total - principalAmount
@@ -10,4 +10,3 @@ interest = total - principalAmount
 print("----- Details Calculated -----")
 print('\nTotal = %0.2f' %total)
 print('\nInterest = %0.2f' %interest)
-

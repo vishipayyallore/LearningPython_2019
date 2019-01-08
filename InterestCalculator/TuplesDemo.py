6dbbf204fecf7b0dacb5553dc8fb43d813c61bc9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-08 23:12:48 +0530

    Simple Tuples

--- InterestCalculator/TuplesDemo.py
+++ InterestCalculator/TuplesDemo.py
@@ -1,9 +1,26 @@
 print("----- Tuples Demo -----")
 
+class DataTypesDemo:
+    Instances = 0
+
+    def __init__(self, tupleObject ):
+        self.tupleObject = tupleObject
+        DataTypesDemo.Instances += 1
+
+    def displayDetails( self ):
+        print("----- DataTypesDemo Details -----")
+        print('DataTypesDemo.Instances: ', self.Instances)
+        print('Tuple Data: ', self.tupleObject)
+        print()
+
 # Defining a tuple without any element
 tupleEmpty = ()
-print(tupleEmpty)
+#print(tupleEmpty)
+dataTypeObject = DataTypesDemo(tupleEmpty)
+dataTypeObject.displayDetails()
 
 tuplePerson = (1, 'Shiva Sai', 24,567.90)
-print(tuplePerson)
+#print(tuplePerson)
+dataTypeObject = DataTypesDemo(tuplePerson)
+dataTypeObject.displayDetails()
 

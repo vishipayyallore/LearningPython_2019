e440d5082d905b0fe28dabc740552148dec91059
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-10 09:12:08 +0530

    Little Enhancements

--- InterestCalculator/TuplesDemo.py
+++ InterestCalculator/TuplesDemo.py
@@ -21,12 +21,33 @@ tupleEmpty = ()
 dataTypeObject = DataTypesDemo(tupleEmpty)
 dataTypeObject.displayDetails()
 
+#  Person Tuples
 tuplePerson = (1, 'Shiva Sai', 24,567.90)
 dataTypeObject = DataTypesDemo(tuplePerson)
 dataTypeObject.displayDetails()
 
-# TODO: Work on nested Tuples
+#  Nested Tuples
 tupleEmployee = (1, 'A1001', 24)
 dataTypeObject = DataTypesDemo( (tuplePerson, tupleEmployee) )
+dataTypeObject.displayDetails()
+
+# Repetition Tuple
+repeatTuple = ( 'Python 3',) * 4
+dataTypeObject = DataTypesDemo(repeatTuple)
+dataTypeObject.displayDetails()
+
+# Slicing with tuples
+sample_tuple = (0 ,1, 2, 3, 4)
+
+withoutFirstItem = sample_tuple[1:]
+dataTypeObject = DataTypesDemo(withoutFirstItem)
+dataTypeObject.displayDetails()
+
+tupleReverse = sample_tuple[::-1]
+dataTypeObject = DataTypesDemo(tupleReverse)
+dataTypeObject.displayDetails()
+
+from3to5Tuple = sample_tuple[2:5]
+dataTypeObject = DataTypesDemo(from3to5Tuple)
 dataTypeObject.displayDetails()
 

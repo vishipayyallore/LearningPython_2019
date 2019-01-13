5a1d729e839d11fb5223b2144e868f43c6f8b3a4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-13 09:44:49 +0530

    changes

--- 13Jan2019/StringsDemo2.py
+++ 13Jan2019/StringsDemo2.py
@@ -10,15 +10,25 @@ class StringDataTypeDemo:
         print( f"Value: {value}")
 
 
-title = "String Demo"
+title = "Indexing String Demo"
 stringDemo = StringDataTypeDemo()
 
 name = "Shiva Sai"
 stringDemo.displayDetails(title, name)
 
-# Strings can be indexed (subscripted), with the first character having index 0. There is no separate character type; 
+# Strings can be indexed (starting 0). There is no separate character type; 
 stringDemo.displayDetails(title, name[0])
 
 # Indices may also be negative numbers, to start counting from the right. negative indices start from -1.
 stringDemo.displayDetails(title, name[-3] + name[-2] + name[-1] )
+
+title = "Slicing String Demo"
+
+# slicing of Strings
+stringDemo.displayDetails(title, name[0:-3])
+stringDemo.displayDetails(title, name[0:len(name)])
+stringDemo.displayDetails(title, name[0:])
+stringDemo.displayDetails(title, name[:-1])
+stringDemo.displayDetails(title, name[-3:])
+
 

91e0ca9f30f0b97d2b2fa013160c0825b42cd706
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-19 20:38:41 +0530

    Moved the method into class

--- 16Jan2019/FibonacciDemo.py
+++ 16Jan2019/FibonacciDemo.py
@@ -1,9 +1,31 @@
 # Fibonacci series
 
-first, second = 0, 1
+class FibonacciSeriesDemo:
+    Instances = 0
 
-while first < 10:
-    print(first)
-    print(f'----- {first} {second} -----')
-    first, second = second, first+second
+    def __init__( self ):
+        FibonacciSeriesDemo.Instances += 1
+
+    def displayFibonacci( self, title, value ):
+        first, second = 0, 1
+        print(f"----- {title}till {value} -----")
+        print( f'FibonacciSeriesDemo.Instances: {self.Instances}')
+        while first < value:
+            print(first)
+            # print(f'----- {first} {second} -----')
+            first, second = second, first+second
+
+title = "Fibonacci Series Demo"
+
+fibonacci = FibonacciSeriesDemo()
+fibonacci.displayFibonacci(title, 10)
+
+fibonacci.displayFibonacci(title, 20)
+
+# first, second = 0, 1
+
+# while first < 10:
+    # print(first)
+    # print(f'----- {first} {second} -----')
+    # first, second = second, first+second
 

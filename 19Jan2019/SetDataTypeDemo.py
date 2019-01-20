75f3d03a2a1fa048c4bb5c96d4b08ccfa404da4d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-20 11:55:18 +0530

    more samples

--- 19Jan2019/SetDataTypeDemo.py
+++ 19Jan2019/SetDataTypeDemo.py
@@ -8,15 +8,27 @@ class SetDataTypeDemo:
     def displaySetValues(self, title, value):
         print(f"----- {title} -----")
         print(f'SetDataTypeDemo.Instances: {self.Instances}')
-        print(f'----- {value} -----')
+        print(f'{value}')
 
 title = "Set Data Type Demo"
 
 setDataType = SetDataTypeDemo()
 
+# set can't store duplicate
 numbersSet = { 3, 7, 11, 15 }
 setDataType.displaySetValues(title, numbersSet)
 
 # Mixed data types set
 mixedDataSet = {11, 1.1, "11", (1, 2)}
 setDataType.displaySetValues(title, mixedDataSet)
+
+# using set() method
+mixedDataSet1 = set([11, 1.1, "11", "Shiva", (1, 2)])
+setDataType.displaySetValues(title, mixedDataSet1)
+
+listData = [11, 1.1, "11", "Shiva Sai", (1, 2)]
+listData.append("Rajesh")
+setDataType.displaySetValues("Display List", listData)
+
+mixedDataSet2 = set(listData) 
+setDataType.displaySetValues(title, mixedDataSet2)

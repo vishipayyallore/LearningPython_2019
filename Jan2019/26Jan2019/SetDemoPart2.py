e5a53a5f25d66a0ce4bc70e5b4427ac66f21575f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-26 23:55:14 +0530

    new program

---
+++ Jan2019/26Jan2019/SetDemoPart2.py
@@ -0,0 +1,30 @@
+# Set Data Type Demo
+class SetDataTypeDemo:
+    Instances = 0
+
+    def __init__(self):
+        SetDataTypeDemo.Instances += 1
+
+    def displaySetValues(self, title, value):
+        print(f"----- {title} -----")
+        print(f'SetDataTypeDemo.Instances: {self.Instances}')
+        print(f'{value}')
+
+    def displayType(self, title, theObject):
+        print(f"----- {title} -----")
+        print(f'SetDataTypeDemo.Instances: {self.Instances}')
+        print(f'{type(theObject)}')
+
+
+title = "Set Data Type Demo"
+title1 = "Identify Data Type Demo"
+
+setDataType = SetDataTypeDemo()
+
+# Empty Set
+emptySet = {}
+emptySet1 = set()
+
+setDataType.displayType(title1, emptySet)
+setDataType.displayType(title1, emptySet1)
+

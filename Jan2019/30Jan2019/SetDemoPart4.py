11aaddee825070097e80cae66b7a5ee714eb4466
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-30 09:54:25 +0530

    for loop

--- Jan2019/30Jan2019/SetDemoPart4.py
+++ Jan2019/30Jan2019/SetDemoPart4.py
@@ -21,19 +21,40 @@ class SetDataTypeDemo:
         except Exception as error:
             print("Error in SetDataTypeDemo::displaySetValues:", error)
 
+    def displayIndividualElements(self, title, theSetOfElements):
+        try:
+            print(f"----- {title} -----")
+            print(f'SetDataTypeDemo.Instances: {self.Instances}')
+            for currentElement in theSetOfElements:
+                print(currentElement)
+        except Exception as error:
+            print("Error in SetDataTypeDemo::displayIndividualElements:", error)
 
+
+# --------------------------------------------------------------------------------
 title = "Set Data Type Demo"
 
 setDataType = SetDataTypeDemo()
 
-oddNumbers = {1, 3, 5, 7, 9}
-evenNumbers = {2, 4, 6, 8, 0}
+oddNumbers = {1, 3, 5, 7, 9, 22}
+evenNumbers = {2, 4, 6, 8, 0, 22}
 
 setDataType.displaySetValues(title, oddNumbers)
 setDataType.displaySetValues(title, evenNumbers)
 setDataType.displaySetValues(title, (oddNumbers | evenNumbers))
 
 setDataType.displaySetValues("Union", oddNumbers.union(evenNumbers))
-setDataType.displaySetValues("Intersection", oddNumbers.intersection(evenNumbers))
-#setDataType.displaySetValues("Union", oddNumbers.union(evenNumbers))
-#setDataType.displaySetValues("Union", oddNumbers.union(evenNumbers))
+
+setDataType.displaySetValues(
+    "Intersection", oddNumbers.intersection(evenNumbers))
+setDataType.displaySetValues("Intersection", oddNumbers & evenNumbers)
+
+setDataType.displaySetValues("Difference", oddNumbers - evenNumbers)
+setDataType.displaySetValues(
+    "Difference", oddNumbers.difference(evenNumbers))
+
+setDataType.displaySetValues("Symmetric Difference", oddNumbers ^ evenNumbers)
+setDataType.displaySetValues(
+    "Symmetric Difference", oddNumbers.symmetric_difference(evenNumbers))
+
+setDataType.displayIndividualElements("Display Individual Elements", oddNumbers)

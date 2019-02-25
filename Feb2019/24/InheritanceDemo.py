ff2ef2a4b271504281a310e72c0fdbc1fc2fdddc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-25 09:14:12 +0530

    Simple update

--- Feb2019/24/InheritanceDemo.py
+++ Feb2019/24/InheritanceDemo.py
@@ -1,15 +1,15 @@
-# String Formatting Demo
-class StringFormattingDemo:
+# Inheritance "is a " Relationship
+class Shape:
     Instances = 0
 
-    def __init__(self):
-        StringFormattingDemo.Instances += 1
+    def __init__(self, area):
+        Shape.Instances += 1
+        self.__area__ = area
 
-    def formatSingleString(self, title, value):
+    def displayDetails(self):
         try:
-            print(
-                f"----- {title} | Instances: {self.Instances}-----")
-            print(f'StringFormattingDemo.Instances: {self.Instances}')
-            print('{}'.format(value))
+            print(f"----- Shape | Instances: {self.Instances}-----")
+            print(f'Shape.Instances: {self.Instances}')
+            print('{}'.format(self.__area__))
         except Exception as error:
-            print("Error in StringFormattingDemo::formatSingleString:", error)
+            print("Error in Shape::formatSingleString:", error)

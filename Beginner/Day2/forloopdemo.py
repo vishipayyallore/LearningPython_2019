a9b10ab9ecfd737e9e7b657521ed9791d39ddb91
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-20 01:05:55 +0530

    for loop

---
+++ Beginner/Day2/forloopdemo.py
@@ -0,0 +1,15 @@
+
+for counter in range(11):
+    print(counter, end=' ')
+
+print()
+for counter in range(1, 11):
+    print(counter, end=' ')
+
+
+print()
+for counter in range(0, 101, 5):
+    print(counter, end=' ')
+else:
+    print('\nFinished the for loop')
+

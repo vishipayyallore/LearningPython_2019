c0db411fd6867f25de536008d1f2832d530016a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-20 00:06:40 +0530

    Few other changes

--- Beginner/Day2/functiondemo1.py
+++ Beginner/Day2/functiondemo1.py
@@ -8,17 +8,21 @@ def add_two_numbers(value1, value2):
 
 def add_two_numbers_v2(value1: int, value2: int) -> int:
     return value1 + value2
-
+    
 
 def main():
     
-    value1 = 10
-    value2 = 20
+    value1 = int(input("Enter Value 1: "))
+    value2 = int(input("Enter Value 2: "))
 
     results = add_two_numbers(value1, value2)
-    print(f"Sum of {value1} + {value2} = {results}")
+    print(f"Sum (V1) of {value1} + {value2} = {results}")
 
     results = add_two_numbers_v2(value1, value2)
+    print(f"Sum  (V2) of {value1} + {value2} = {results}")
+
+    # We will use this scnario for Exception handling
+    results = add_two_numbers_v2(value1, "S")
     print(f"Sum of {value1} + {value2} = {results}")
 
 # Invoking the main method.

367b32e0a43ea00b5e33f77bcb506165288dc76e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-20 08:28:02 +0530

    small enhancements

--- Beginner/Day2/stringsdemo2.py
+++ Beginner/Day2/stringsdemo2.py
@@ -7,6 +7,15 @@ def show_all_characters(stringvalue: str):
     show_banner('=', 100)
     
     for character in stringvalue:
+        print(character)
+    
+    show_banner('-', 100)
+
+
+def show_all_characters_v2(stringvalue: str):
+    show_banner('=', 100)
+    
+    for character in stringvalue:
         print(character, sep=' ', end = ' ')
     
     print('\n')
@@ -15,11 +24,16 @@ def show_all_characters(stringvalue: str):
 
 
 def main():
-    show_all_characters("Shiva Sai")
+    show_all_characters("ABCD")
 
-    show_all_characters("Mohd Azim")
+    # To Showcase Error condition. Wrong Function Name
+    # show_all_characters_("ABCD")
 
-    show_all_characters("Samule Mathews")
+    show_all_characters_v2("Shiva Sai")
+
+    show_all_characters_v2("Mohd Azim")
+
+    show_all_characters_v2("Samule Mathews")
 
 
 # Invoking main method.

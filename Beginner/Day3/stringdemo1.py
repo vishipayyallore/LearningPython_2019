c755b19c0888b9caf2a3926cd61bfe1e8c7a2270
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-21 12:11:19 +0530

    Updates

--- Beginner/Day3/stringdemo1.py
+++ Beginner/Day3/stringdemo1.py
@@ -4,14 +4,14 @@
 
 def show_characters_from_positivieindex(string_value):
     for index in range(len(string_value)):
-        print(string_value[index], end=' ')
+        print(string_value[index], end=' | ')
     print()
 
 
 def show_characters_from_negativeindex(string_value):
     endcount = -1 * (len(string_value) + 1)
     for index in range(-1, endcount, -1):
-        print(string_value[index], end=' ')
+        print(string_value[index], end=' | ')
     print()
 
 

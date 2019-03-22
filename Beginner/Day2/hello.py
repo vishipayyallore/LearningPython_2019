54de5306e76942e0e5f72e0ba1cb2e540f2bfc43
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-22 07:15:41 +0530

    small change

---
+++ Beginner/Day2/hello.py
@@ -0,0 +1,13 @@
+#! C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe
+
+import sys
+
+print(sys.executable)
+print(sys.version)
+print(sys.prefix)
+print(sys.path)
+
+print("Hello World")
+
+#name = input("Enter your Name: ")
+

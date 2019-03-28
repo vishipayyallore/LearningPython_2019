afc9b03eccd266db3b76eba442dbc01ec4bedeb9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-29 01:30:40 +0530

    xml demo

--- Beginner/Day6/jsondemo1.py
+++ Beginner/Day6/jsondemo1.py
@@ -1,6 +1,5 @@
 import json
 from pprint import pprint as pp
-
 from utilitiesmodule import *
 
 filepath = './datastore/movies.json'
@@ -18,5 +17,5 @@ banner_v2(100)
 
 banner("Movie Titles", 100)
 for movie in json_content:
-    print(movie['title'])
+    print(movie['title'], ' | ', movie['year'])
 banner_v2(100)
---
+++ Beginner/Day6/xmldemo1.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as etree
+from pprint import pprint as pp
+from utilitiesmodule import *
+
+filepath = './datastore/persons.xml'
+
+xml_content = etree.parse(filepath)
+
+banner("Pretty Print", 100)
+
+for person in xml_content.findall('.//person'):
+    print(person.find('firstName').text, end = ' | ')
+    print(person.find('lastName').text, end = ' | ')
+    print(person.find('age').text)
+    
+banner_v2(100)

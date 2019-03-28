7cc367117c161e4356c08a8c5af54021cede712e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-29 01:18:54 +0530

    json files

---
+++ Beginner/Day6/jsondemo1.py
@@ -0,0 +1,22 @@
+import json
+from pprint import pprint as pp
+
+from utilitiesmodule import *
+
+filepath = './datastore/movies.json'
+
+with open(filepath, mode='r', encoding='utf-8') as jsonfile:
+    json_content = json.load(jsonfile)
+
+banner("Simple Print", 100)
+print(json_content)    
+banner_v2(100)
+
+banner("Pretty Print", 100)
+pp(json_content)    
+banner_v2(100)
+
+banner("Movie Titles", 100)
+for movie in json_content:
+    print(movie['title'])
+banner_v2(100)

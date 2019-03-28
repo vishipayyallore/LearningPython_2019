3458bb088ad88e818e125ab32fb4ba5d86c5bfc7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-29 00:28:07 +0530

    First GUI Program

--- Beginner/Day6/firstgui.py
+++ Beginner/Day6/firstgui.py
@@ -1,6 +1,13 @@
-import tkinter as tk
+from tkinter import *
 
-win = tk.Tk()
+win = Tk()
 win.title('Hello Python')
+win.geometry("250x100")
+
+label = Label(win, text="Hello Python", font="Verdana 10 bold", fg = "blue")
+label.pack()
+
+button = Button(win, text="Click Here!", font="Verdana 10 bold", fg = "green")
+button.pack()
 
 win.mainloop()

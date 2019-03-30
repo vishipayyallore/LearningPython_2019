488c57548d864e1ef3bf3d464c4d4b4acb15d4bb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-30 12:56:52 +0530

    Small modifications

--- Beginner/Day6/simplefiledemo1.py
+++ Beginner/Day6/simplefiledemo1.py
@@ -10,19 +10,16 @@ def print_students_titlecase():
         print(stud)
 
 
-def add_student(name, roll_number=332, student_age=18):
-    student = {"roll_number": roll_number,
-               "name": name, "student_age": student_age}
-    students.append(student)
-    return student
-
-
 def save_file(student):
     try:
         f = open(filepath, "a")
         f.write(
             f'{student["roll_number"]} {student["name"]} {student["student_age"]}\n')
         f.close()
+
+        student = {"roll_number": student["roll_number"],
+                   "name": student["name"], "student_age": student["student_age"]}
+        students.append(student)
     except Exception as error:
         print("Could not save file ", error)
 
@@ -33,7 +30,7 @@ def read_file():
 
         for student in f.readlines():
             values = student.split()
-            add_student(values[0], values[1], values[2])
+            # add_student(values[0], values[1], values[2])
 
         f.close()
     except Exception:
@@ -48,7 +45,7 @@ roll_number = input("Enter student ID: ")
 student_name = input("Enter student name: ")
 student_age = input("Enter student age: ")
 
-student = add_student(student_name, roll_number, student_age)
+# student = add_student(student_name, roll_number, student_age)
+student = {"roll_number": roll_number, "name": student_name, "student_age": student_age}
+
 save_file(student)
-
-

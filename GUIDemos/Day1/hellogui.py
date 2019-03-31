7c84817fa437ca0ca69dab93e32a16cfee3c0dec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-31 23:02:51 +0530

    Changing Backgroud and fore groud colors

--- GUIDemos/Day1/hellogui.py
+++ GUIDemos/Day1/hellogui.py
@@ -3,11 +3,12 @@ from tkinter import *
 win = Tk()
 win.title('Hello Python')
 win.geometry("250x100")
+win.configure(bg = 'light blue')
 
-label = Label(win, text="Hello Python", font="Verdana 10 bold", fg = "blue")
+label = Label(win, text="Hello Python", font="Verdana 10 bold", fg = "blue", bg = 'light blue')
 label.pack()
 
-button = Button(win, text="Click Here!", font="Verdana 10 bold", fg = "green")
+button = Button(win, text="Close Window!", font="Verdana 10 bold", fg = "orange red", command=win.quit, bg = 'dodger blue')
 button.pack()
 
 win.mainloop()

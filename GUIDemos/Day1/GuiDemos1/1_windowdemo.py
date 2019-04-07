cc727019862363000e372d38668d4ac19b097f68
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-07 21:51:07 +0530

    Lable Demo

--- GUIDemos/Day1/GuiDemos1/1_windowdemo.py
+++ GUIDemos/Day1/GuiDemos1/1_windowdemo.py
@@ -2,18 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
-window.title('Button Demo')
+window.title('Window Demo')
 window.geometry("300x80")
 window.configure(bg='light blue')
-
-button_text = tk.StringVar()
-button_text.set('Enable')
-
-def enable_disable():
-    button_text.set('Enable' if button_text.get() == 'Disable' else 'Disable')
-
-button_enable_disable = ttk.Button(window, textvariable=button_text, command=enable_disable)
-button_enable_disable.pack()
-
 
 window.mainloop()

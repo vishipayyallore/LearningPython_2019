ed399a7083978dd45078f4b91d1317c660c77e06
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-17 23:14:59 +0530

    Retrieving the value from XML Content

--- Beginner/Day6/xmldemo1.py
+++ Beginner/Day6/xmldemo1.py
@@ -2,9 +2,16 @@ import xml.etree.ElementTree as etree
 from pprint import pprint as pp
 from utilitiesmodule import *
 
-filepath = './datastore/persons.xml'
+# filepath = './datastore/persons.xml'
+filepath = './datastore/c2gen5_beta_t.xml'
+
 
 xml_content = etree.parse(filepath)
+
+control1 = xml_content.findall('GEN_CONFIG_TBL/FORMAT_CONTROL_1') 
+
+char_format = xml_content.findall('GEN_CONFIG_TBL/FORMAT_CONTROL_1/CHAR_FORMAT')
+print(f'Value: {char_format[0].text}')
 
 banner("Pretty Print", 100)
 

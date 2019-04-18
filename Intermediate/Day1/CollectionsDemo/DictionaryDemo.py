eb6a6de193b420b16ca1134e81f49de933e02d21
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-18 23:36:56 +0530

    Collections Demos

---
+++ Intermediate/Day1/CollectionsDemo/DictionaryDemo.py
@@ -0,0 +1,53 @@
+# Dictionary Data Type Demo
+class DictDataTypeDemo:
+    Instances = 0
+
+    def __init__(self):
+        DictDataTypeDemo.Instances += 1
+
+    def displayDictionaryValues(self, title, value):
+        try:
+            print(
+                f"----- {title} | Length: {len(value)} | Instances: {self.Instances}-----")
+            print(f'DictDataTypeDemo.Instances: {self.Instances}')
+            print(f'{value}')
+        except Exception as error:
+            print("Error in DictDataTypeDemo::displaySetValues:", error)
+
+    def displayIndividualElements(self, title, theSetOfElements):
+        try:
+            print(f"----- {title} | Instances: {self.Instances} -----")
+            for currentKey in theSetOfElements:
+                print(f'{currentKey} = {theSetOfElements[currentKey]}')
+        except Exception as error:
+            print("Error in DictDataTypeDemo::displayIndividualElements:", error)
+
+
+# --------------------------------------------------------------------------------
+title = "Dictionary Data Type Demo"
+title1 = "Individual Display Demo"
+dictDataType = DictDataTypeDemo()
+
+# Empty Dictionary
+emptyDict = {}
+dictDataType.displayDictionaryValues(title, emptyDict)
+dictDataType.displayIndividualElements(title1, emptyDict)
+
+friuts = {1: 'Apple', 2: 'Orange'}
+dictDataType.displayDictionaryValues(title, friuts)
+dictDataType.displayIndividualElements(title1, friuts)
+
+family = {'id': 1, 'name': "Shiva", 'salary': 12345.67,
+          'subjects': ['CSharp', 'Angular']}
+dictDataType.displayDictionaryValues(title, family)
+dictDataType.displayIndividualElements(title1, family)
+
+# from dict() method. It removes the duplicate items.
+friuts = dict({1: 'Apple', 2: 'Orange', 3: 'Grapes', 'Three': 'Grapes', 3: 'Grapes'})
+dictDataType.displayDictionaryValues(title, friuts)
+dictDataType.displayIndividualElements(title1, friuts)
+
+# from dict() method using Tuples
+friuts = dict([(1, 'Apple'), (2, 'Orange'), (3, 'Grapes'), (4, 'Mango'), (5, 'Pine Apple')])
+dictDataType.displayDictionaryValues(title, friuts)
+dictDataType.displayIndividualElements(title1, friuts)
---
+++ Intermediate/Day1/CollectionsDemo/ListDemos.py
@@ -0,0 +1,39 @@
+class ListDataTypeDemo:
+    Instances = 0
+
+    def __init__(self):
+        ListDataTypeDemo.Instances += 1
+
+    def displayDetails(self, title, value):
+        print(f"----- {title} -----")
+        print(f'ListDataTypeDemo.Instances: {self.Instances}')
+        print(f"Length: {len(value)}")
+        print(f"Value: {value}")
+
+
+title = "List Data Type Demo"
+
+listDemo = ListDataTypeDemo()
+
+# Empty List
+listOne = []
+listDemo.displayDetails(title, listOne)
+
+integerList = [10, 20, 30]
+listDemo.displayDetails(title, integerList)
+
+squares = [1, 4, 9, 16, 25]
+listDemo.displayDetails(title, squares)
+
+listDemo.displayDetails("List Slicing", squares[2:4])
+
+listDemo.displayDetails("List Slicing", squares[:])
+
+listDemo.displayDetails("List Concatenation", squares + integerList)
+
+# Nested List
+lowerCase = ['a', 'b', 'c', 'd']
+upperCase = ['A', 'B', 'C', 'D']
+numbers = [1, 2, 3, 4]
+universalSet = [lowerCase, upperCase, numbers]
+listDemo.displayDetails("Nested List", universalSet)

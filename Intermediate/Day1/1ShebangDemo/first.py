5e3ab15eed4fee7bac038b09493b257644125e43
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-21 19:49:46 +0530

    Changed the Import

--- Intermediate/Day1/1ShebangDemo/first.py
+++ Intermediate/Day1/1ShebangDemo/first.py
@@ -9,7 +9,7 @@ import sys
 import datetime
 
 # Need to mention the path
-sys.path.insert(0, '../ModulesAndPyCache')
+sys.path.insert(0, '../2ModulesAndPyCache')
 from utilitiesmodule import banner
 
 
@@ -18,12 +18,14 @@ class Person:
     # Calculating the date
     today = datetime.datetime.now().strftime('%c')
 
+
     def get_person_details(self):
         # Accepting Inputs from User
         self.name = input("Enter Your Name: ")
         self.employee_id = int(input("Enter Employee Id: "))
         self.salary = float(input("Enter Salary: "))
         self.is_manager = bool(input("Enter Manager Status: "))
+
 
     def display_person_details(self):
         title = f"| Employee Details | Date: {self.today} |"

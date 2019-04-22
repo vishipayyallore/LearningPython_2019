cf235a9403d86e99bbf47f0a529909fefc0a99d3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-22 12:13:42 +0530

    ..

--- Intermediate/Day1/practice.py
+++ Intermediate/Day1/practice.py
@@ -1,2 +1,26 @@
 """This File is used for practice the programs daily
 """
+
+class PersonV4:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f'Name: {self.name}, Age: {self.age}')
+
+    def __str__(self):
+        return f'Name: {self.name}, Age: {self.age}'
+
+    # Returns String Representation
+    def __repr__(self):
+        return self.__str__()
+
+
+person = PersonV4(age=21, name='Mathews')
+person.display_details()
+print(person)
+print(repr(person))
+print(person.__str__())
+print(person.__repr__())

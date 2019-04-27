1b1f84f91f7b6d69b5617756a6754008e50aa9f4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-04-27 21:20:39 +0530

    Retrieving the content

---
+++ Intermediate/Day4/6RequestsDemos/get_web_content.py
@@ -0,0 +1,35 @@
+from urllib.request import urlopen
+
+
+
+def get_content(request_url):
+    webcontent = []
+
+    with urlopen(request_url) as content:
+        for line in content:
+            webcontent.append(line.decode('utf-8').split())
+
+    return webcontent
+    
+
+
+
+def main():
+    remote_url = 'http://sixty-north.com/c/t.txt'
+    contents = get_content(remote_url)
+    print(contents)
+
+    remote_url = 'https://www.python.org/'
+    contents = get_content(remote_url)
+    print(contents)
+
+    remote_url = 'http://sixty-north.com/c/t.txt'
+    contents = get_content(remote_url)
+    print(contents)
+
+    remote_url = 'http://go.codeschool.com/spamvanmenu'
+    contents = get_content(remote_url)
+    print(contents)
+
+
+main()

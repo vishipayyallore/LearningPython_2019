1e5b06906526a74712343ed0f72cc8b68469f107
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-06 21:50:43 +0530

    list compressions

---
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo1.py
@@ -0,0 +1,16 @@
+
+numbers = [i for i in range(1,11)]
+print(numbers)
+
+numbers = [i*2 for i in range(1,11)]
+print(numbers)
+
+numbers = [i*i*i for i in range(1,11)]
+print(numbers)
+
+
+numbers = [i for i in range(1,11) if i%2 == 0]
+print(numbers)
+
+numbers = [i for i in range(1,11) if i%2 != 0]
+print(numbers)

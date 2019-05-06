647784fff29785adc3fb5d1a37755f2fe1c1d5d4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-06 22:09:28 +0530

    some more list compresions

--- Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo1.py
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo1.py
@@ -14,3 +14,6 @@ print(numbers)
 
 numbers = [i for i in range(1,11) if i%2 != 0]
 print(numbers)
+
+numbers = [i**2 for i in range(1,11)]
+print(f'Squares: {numbers}')
---
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo2.py
@@ -0,0 +1,10 @@
+names = ["shiva", "sai", "azim", "mathews", "philips", "samule"]
+
+items = [name.capitalize() for name in names]
+
+print(items)
+
+
+string = "Shiva1 Sai2 Azim3 4 5"
+numbers = [x for x in string if x.isdigit()]
+print(numbers)

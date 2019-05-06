565e81209b8e447a6f31ef47cac6b14a24fd0496
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-06 22:19:36 +0530

    list comprehensions

--- Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo2.py
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo2.py
@@ -8,3 +8,12 @@ print(items)
 string = "Shiva1 Sai2 Azim3 4 5"
 numbers = [x for x in string if x.isdigit()]
 print(numbers)
+
+def get_list_comprehensions(lambda_expression, value):
+    return [lambda_expression(x) for x in range(value)]
+
+data = get_list_comprehensions(lambda x: x*2, 12)
+print(data)
+
+data = get_list_comprehensions(lambda x: x**2, 12)
+print(data)

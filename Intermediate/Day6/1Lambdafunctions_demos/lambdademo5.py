67bf30f7ae83d7ede252c680c9e480710928acc3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-08 18:24:33 +0530

    Small modifications

--- Intermediate/Day6/1Lambdafunctions_demos/lambdademo5.py
+++ Intermediate/Day6/1Lambdafunctions_demos/lambdademo5.py
@@ -4,10 +4,11 @@ def modify_data(lambda_expression, listdata):
 
 def main():
     names = ['shiva', 'sai', 'Azim', 'Mathews', 'samules']
+    numbers = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+
     capitalize_names = modify_data(lambda x: x.capitalize(), names)
     print(capitalize_names)
 
-    numbers = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
     multiply_by_two = modify_data(lambda x: x*2, numbers)
     print(multiply_by_two)
 

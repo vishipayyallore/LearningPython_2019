7e703822c6a862f3923207762c13677a3d268066
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-08 21:12:12 +0530

    Few changes to list comprehensions

--- Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo2.py
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo2.py
@@ -6,11 +6,6 @@ print(items)
 items = [len(name) for name in names]
 print(items)
 
-
-string = "Shiva1 Sai2 Azim3 4 5"
-numbers = [x for x in string if x.isdigit()]
-print(numbers)
-
 def get_list_comprehensions(lambda_expression, value):
     return [lambda_expression(x) for x in range(value)]
 
---
+++ Intermediate/Day6/3ListComprehensionsDemos/listcomprehensiondemo3.py
@@ -0,0 +1,11 @@
+
+numbers = [i for i in range(1,11) if i%2 == 0]
+print(numbers)
+
+numbers = [i for i in range(1,11) if i%2 != 0]
+print(numbers)
+
+string = "Shiva1 Sai2 Azim3 4 5"
+numbers = [x for x in string if x.isdigit()]
+print(numbers)
+

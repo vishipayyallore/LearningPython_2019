a52c5c8498cdd5f21855c3a1204b1ad8c687db35
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-13 18:12:42 +0530

    Using Module from centralized location

--- Session1/Day7/usemodules1.py
+++ Session1/Day7/usemodules1.py
@@ -1,2 +1,7 @@
-import ../CustomModules/bannermodule
+import sys
 
+sys.path.append('C:\\LordKrishna\\GitHub\\LearningPython_2019\\Session1\\CustomModules')
+
+import bannermodule
+
+print("Hello World")

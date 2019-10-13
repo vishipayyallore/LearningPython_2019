3d9e8b98659e8be0970a5467eb0e698422689795
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-13 18:20:39 +0530

    few changes

--- Session1/Day7/usemodules1.py
+++ Session1/Day7/usemodules1.py
@@ -1,7 +1,15 @@
 import sys
-
 sys.path.append('C:\\LordKrishna\\GitHub\\LearningPython_2019\\Session1\\CustomModules')
 
 import bannermodule
 
+program_title = 'Using Modules'
+top_header = '='
+bottom_header = '-'
+function_footer = '+'
+count = 50
+
+bannermodule.create_banner('Addition of Numbers', top_header, bottom_header, count)
 print("Hello World")
+
+
